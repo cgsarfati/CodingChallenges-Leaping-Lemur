6eb5e603786a20f8f4a25c8cbf2e7be0545d9d1b
vagrant <vagrant@vagrant.vm>
2017-10-08 21:34:45 +0000

    solved brute force problem

--- lemur.py
+++ lemur.py
@@ -23,6 +23,8 @@ It cannot use deadly branch (1 in the list).
     5
 """
 
+# BRUTE FORCE
+
 
 def lemur(branches):
     """Return number of jumps needed."""
@@ -30,12 +32,30 @@ def lemur(branches):
     assert branches[0] == 0, "First branch must be alive"
     assert branches[-1] == 0, "Last branch must be alive"
 
+    num_jumps = 0
+    current = 0
+
+    if len(branches) == 2:
+        return 1
+
+    while current != len(branches) - 1:
+        peek = current + 2
+        if branches[peek] == 0:
+            num_jumps += 1
+            current = peek
+        else:
+            num_jumps += 1
+            current += 1
+
+    return num_jumps
+
     # 0 = alive, 1 = dead
     # 1st and last always alive
     # jumping to end always possible
     # default - make lemur jump 2; if lands on dead, jump 1
     # counter for each iteration (2x/1x = 1 jump); return at end
-    # need to know value two items ahead for each iteration
+    # need to know current value inside each iteration 
+    # need to peek value two items ahead within each iteration (enumerate?)
 
 if __name__ == '__main__':
     import doctest
